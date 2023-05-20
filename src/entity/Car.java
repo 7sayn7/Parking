abb0f90ac1b7166da7e91aa69f9788ca552a71b4
7sayn7 <Nikolai.Kasatkin@urfu.me>
2023-05-21 01:04:30 +0500

    update car

--- src/entity/Car.java
+++ src/entity/Car.java
@@ -1,4 +1,49 @@
 package entity;
 
+import managers.ParkingLotManager;
+
+import java.util.Random;
+
 public class Car {
+    int number;
+    int countOfSteps;
+    static Random random = new Random();
+
+    public Car(int countOfSteps) {
+        this.number = generateNumber();
+        this.countOfSteps = countOfSteps;
+    }
+
+    public int generateNumber() {
+        boolean isUniq = false;
+        int a = 0;
+        while (!isUniq) {
+            a = random.nextInt(9000) + 1000;
+            for (int i = 0; i < ParkingLotManager.parking.cars.length; i++) {
+                if (a == ParkingLotManager.parking.cars[i].getNumber()) {
+                    break;
+                }
+                if (i == ParkingLotManager.parking.cars.length - 1) {
+                    isUniq = true;
+                }
+            }
+        }
+        return a;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public int getCountOfSteps() {
+        return countOfSteps;
+    }
+
+    public void setCountOfSteps(int countOfSteps) {
+        this.countOfSteps = countOfSteps;
+    }
 }
--- src/managers/ParkingLotManager.java
+++ src/managers/ParkingLotManager.java
@@ -1,8 +1,17 @@
 package managers;
 
+import entity.Parking;
+
+import java.util.Scanner;
+
 public class ParkingLotManager {
 
+    static Scanner in = new Scanner(System.in);
+    public static Parking parking = new Parking(in.nextInt());
+
     public static void start() {
+
+
         while (true) {
         }
     }
